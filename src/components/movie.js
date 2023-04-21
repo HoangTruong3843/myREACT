eb8446b9945b3adffe99632c1880c48415163a29
Your Name <you@example.com>
2023-04-21 17:42:54 -0600

    new updates

--- src/components/movie.js
+++ src/components/movie.js
@@ -7,14 +7,14 @@ import MovieDetail from "../components/moviedetail"
 function Movie(props) {
     const [selectedMovie] = useState(props.selectedMovie);
     const params = useParams();
-    const Movie_ID = params.Movie_ID;
+    const Movie_ID = params.movieId;
     console.log(Movie_ID);
     const dispatch = useDispatch();
     if (selectedMovie == null) {
         dispatch(fetchMovie(Movie_ID));
     }
 
-    return (<MovieDetail Movie_ID={Movie_ID} />)
+    return (<MovieDetail movieId={Movie_ID} />)
 }
 
 export default Movie;
